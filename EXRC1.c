9b8fc35307aa1dfe5958d21a4487fcefcae674b3
patrickdeoliveira08-collab <patrickdeoliveira08@gmail.com>
2026-02-24 20:06:30 +0000

    LessERRORCode

---
+++ EXRC1.c
@@ -0,0 +1,11 @@
+// Patrick de Oliveira Araujo RA:10739149 Lab 1. Ex 1 //
+
+#include <stdio.h>
+
+int main() {
+    printf("Mackenzie - FCI\n");
+    printf("Ciência da Computação\n");
+    printf("Algoritmos e Prog. II Turma 02N12\n");
+   
+    return 0;
+}
